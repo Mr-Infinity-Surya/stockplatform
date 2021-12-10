f9f9c0b630497086fe01497f93e29429d01f2de5
Shru-04 <shrujanraghu@gmail.com>
2021-12-10 11:31:11 +0530

    update

--- logins/views.py
+++ logins/views.py
@@ -30,7 +30,7 @@ def user_login(request):
         #logout(request)
         if user is not None:
              login(request,user)
-             return render(request,'index.html')
+             return render(request,'index.html',context)
         else:
              return HttpResponse("<h1> Invalid Credentials </h1>")
     return render(request,template_name,context)
@@ -65,7 +65,7 @@ def user_reg(request):
             user.last_name = form.cleaned_data['last_name']
             user.set_password(password)
             user.save()
-            return HttpResponse("<h1> Its correct <a href = 'register'> go back </a></h1>")
+            return render(request,'index.html',context)
     else:
         form = UserForm()
         HttpResponse("Invalid <a href = 'register'> go back </a>")
