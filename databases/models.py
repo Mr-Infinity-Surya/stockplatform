70a6ee2e1ebc02666bca862b79c6e4e8a6ed56a2
shrujanr-cs19 <shrujanr.cs19@rvce.edu.in>
2021-12-12 23:30:19 +0530

    up

--- databases/models.py
+++ databases/models.py
@@ -1,3 +1,5 @@
+from django import forms
+from django.core.validators import validate_email
 from django.db import models
 
 # Create your models here.
@@ -21,26 +23,25 @@ class Stock(models.Model) :
     growth =  models.FloatField()
     company_name = models.IntegerField()
 class Investor(models.Model):
-    ID = models.IntegerField(primary_key=True)
-    FirstName = models.CharField(max_length=1000)
-    LastName = models.CharField(max_length=1000)
-    No_of_Investments = models.IntegerField()
-    Contact_no = models.CharField(max_length=13)
-    Pan_card_no = models.CharField(max_length=1000)
+    UserName = models.CharField(max_length=1000,primary_key=True,unique=True)
+    F_Name = models.CharField(max_length=1000,unique=True)
+    L_Name = models.CharField(max_length=1000,unique=True)
+    Email = models.CharField(max_length=1000,validators=[validate_email],unique=True)
+    Contact_no = models.CharField(max_length=13,unique=True)
+    Pan_card_no = models.CharField(max_length=1000,unique=True)
     Address_line1 = models.CharField(max_length=1000)
     Address_line2 =models.CharField(max_length=1000)
     State = models.CharField(max_length=1000)
     city = models.CharField(max_length=1000)
     District = models.CharField(max_length=1000)
     Pin_code = models.IntegerField()
-    User_login_id = models.IntegerField()
 class Bank(models.Model):
     Account_no = models.CharField(primary_key=True,max_length=1000)
+    Username = models.ForeignKey(Investor,on_delete=models.CASCADE)
     IFSC_code = models.CharField(max_length=1000)
     Branch = models.CharField(max_length=1000)
     Name = models.CharField(max_length=1000)
     Current_amount = models.IntegerField()
-    Investor_id = models.CharField(max_length=1000)
 class Company(models.Model):
     Name = models.CharField(primary_key=True,max_length=1000)
     Website_link = models.CharField(max_length=1000)
