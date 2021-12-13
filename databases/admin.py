3f35f15cc4cd8a1990209fa9fed9717e6e9d07a0
Shru-04 <shrujanraghu@gmail.com>
2021-12-13 11:46:27 +0530

    update

--- databases/admin.py
+++ databases/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Invest)
 admin.site.register(Stock)
 admin.site.register(Investor)
 admin.site.register(Bank)
 admin.site.register(Company)
-admin.site.register(Company_category)
+admin.site.register(Investment)
