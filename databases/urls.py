e6b0a1540ca99d7af596f5081fc6be7ece8f3e9b
Shru-04 <shrujanraghu@gmail.com>
2021-12-16 14:15:52 +0530

    update

--- databases/urls.py
+++ databases/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'databases'
 urlpatterns = [
     path('',views.index,name='index'),
-    path('stocks',views.stock_manage,name='stocks')
+    path('stocks',views.stock_manage,name='stocks'),
+    path('userstock',views.user_stock,name='userstock'),
+    path('filldb',views.fill_db,name='filldb'),
+    path('filldb2',views.fill_db2,name='filldb2'),
 ]
--- databases/views.py
+++ databases/views.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from django.conf.urls import url
 from django.shortcuts import render
 from django.http import HttpResponse
 import yfinance as yf
 import numpy as np
-from databases.models import Stock
+from databases.models import Bank, Company, Investment, Stock
 import plotly.graph_objs as go
 from django.contrib.auth.models import User
 
@@ -31,3 +32,77 @@ def stock_manage(request):
     else:
         return HttpResponse("<h1> Illegal request </h1>")
     # return render(request,'index.html',context)
+
+def user_stock(request):
+    if request.user.is_authenticated:
+        if Stock.objects.filter(ISIN = Investment.objects.get(User_Account_no= Bank.objects.get(Username=request.user.username).Account_no).Stock_ISIN).exists() is True:
+            stocku = Stock.objects.get(ISIN = Investment.objects.get(User_Account_no= Bank.objects.get(Username=request.user.username).Account_no).Stock_ISIN)
+            return HttpResponse("ok<br>{{stocku}}")
+        else:
+            return HttpResponse("Error")
+        
+def get_stock_data(stock_name):
+    data=yf.Ticker(stock_name)
+    info = data.info
+    stk_values = {'currentPrice','beta','volume','regularMarketOpen','revenueGrowth','dayHigh','open','previousClose','dayLow'}
+    cmp_values = {'sector','industry','longBusinessSummary','website','grossProfits','longName'}
+    stk_res = {key: info[key] for key in info.keys() & stk_values}
+    cmp_res = {key: info[key] for key in info.keys() & cmp_values}
+    stk_res['ISIN'] = data.isin
+    stk_res['Name'] = stock_name
+    cmp_res['ISIN'] = stk_res['ISIN']
+    return stk_res,cmp_res
+
+def fill_db(request):
+    if request.user.is_superuser:
+        st = Stock()
+        stk_list = ['AXISBANK.NS','BHARTIARTL.NS','CIPLA.NS','HCLTECH.NS','ICICIBANK.NS','ITC.NS','KOTAKBANK.NS','JSWSTEEL.NS','MARUTI.NS','POWERGRID.NS','SBIN.NS','TATAMOTORS.NS','TATASTEEL.NS','TCS.NS','WIPRO.NS','EICHERMOT.NS','GRASIM.NS', 'HINDUNILVR.NS', 'IOC.NS', 'LT.NS', 'NESTLEIND.NS', 'NTPC.NS','SUNPHARMA.NS', 'TECHM.NS', 'ULTRACEMCO.NS']
+        for i in stk_list:
+            if Stock.objects.filter(Name = i).exists() is True:
+                return HttpResponse("Already updated")
+        for x in stk_list:
+            st = Stock() 
+            stk_res,cmp_res = get_stock_data(x)
+            st.Name = str(stk_res['Name'])
+            st.ISIN = str(stk_res['ISIN'])
+            st.Volume = int(stk_res['volume'])
+            st.Prev_Close = float(stk_res['previousClose'])
+            st.Day_low =  float(stk_res['dayLow'])
+            st.Current_price =  float(stk_res['currentPrice'])
+            st.Beta =  float(stk_res['beta'])
+            st.Regular_market_open =  float(stk_res['regularMarketOpen'])
+            st.Day_high =  float(stk_res['dayHigh'])
+            st.Open =  float(stk_res['open'])
+            st.Revenue_growth =  float(stk_res['revenueGrowth'])
+            print(st)
+            st.clean()
+            st.save()
+        return HttpResponse("ok<br>{{Stock.objects.all}}")
+    else:
+        return HttpResponse("<h1> Hello, ur not supposed to enter HERE !!!!! </h1>")
+
+def fill_db2(request):
+    if request.user.is_superuser:
+        co = Company()
+        stk_list = ['SBIN.NS','TATAMOTORS.NS','TATASTEEL.NS','TCS.NS','WIPRO.NS','EICHERMOT.NS','GRASIM.NS', 'HINDUNILVR.NS', 'IOC.NS', 'LT.NS', 'NESTLEIND.NS', 'NTPC.NS','SUNPHARMA.NS', 'TECHM.NS', 'ULTRACEMCO.NS']   #'AXISBANK.NS','BHARTIARTL.NS','CIPLA.NS','HCLTECH.NS','ICICIBANK.NS','ITC.NS','KOTAKBANK.NS','JSWSTEEL.NS','MARUTI.NS','POWERGRID.NS'
+        for i in stk_list:
+            if Company.objects.filter(Name = i).exists() is True:
+                return HttpResponse("Already updated")
+        for x in stk_list:
+            co = Company() 
+            stk_res,cmp_res = get_stock_data(x)
+            co.Name = str(cmp_res['longName'])
+            co.Stock_ISIN = Stock.objects.get(ISIN = str(cmp_res['ISIN']))
+            co.Sector = str(cmp_res['sector'])    
+            co.Industry = str(cmp_res['industry'])
+            co.Business_Summary =str(cmp_res['longBusinessSummary'])
+            co.Website = cmp_res['website']
+            co.Gross_Profit = float(cmp_res['grossProfits'])
+            print(co)
+            co.clean()
+            co.save()
+        x = Stock.objects.all()
+        return HttpResponse("ok<br>{{x}}")
+    else:
+        return HttpResponse("<h1> Hello, ur not supposed to enter HERE !!!!! </h1>")
+
